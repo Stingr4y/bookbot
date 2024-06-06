6543c22756a2fc768404438b717936c609a711a2
Stingray <stingr4y1985@gmail.com>
2024-06-06 22:50:17 +0300

    Updated main.py to contain countcharacters() function

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = wordcount(text)
+    number_of_letters = countcharacters(text)
     print(f"{num_words} words found in the document")
+    print(f"{number_of_letters} letters found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()    
 
 def wordcount(text):
     words = text.split()
@@ -11,8 +17,15 @@ def wordcount(text):
         number += 1
     return number
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+def countcharacters(text):
+    letters = {}
+    for i in range(0,len(text)):
+        lowered_string = text[i].lower()
+        if lowered_string in letters:
+            character_number = letters[lowered_string] +1
+            letters[lowered_string] = character_number
+        else:
+            letters[lowered_string] = 1   
+    return letters   
 
 main()
